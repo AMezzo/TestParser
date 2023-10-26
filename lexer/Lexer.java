f39ec133ef2621d102d200d91e257e338a1dfa3c
AMezzo <124308783+AMezzo@users.noreply.github.com>
2023-10-26 15:11:57 -0700

    Add files via upload

---
+++ lexer/Lexer.java
@@ -0,0 +1,215 @@
+package lexer;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+import lexer.daos.Symbol;
+import lexer.daos.Token;
+import lexer.daos.TokenKind;
+import lexer.readers.IReader;
+import lexer.readers.SourceFileReader;
+
+public class Lexer implements ILexer, AutoCloseable {
+    private IReader reader;
+    private char ch;
+    private int startPosition, endPosition;
+    private int lineNumber = 1;
+    
+    public Lexer(IReader reader) {
+        this.reader = reader;
+        this.ch = ' ';
+        this.lineNumber = 1; 
+    }
+
+    public Lexer(String sourceFilePath) {
+        this(new SourceFileReader(sourceFilePath));
+    }
+
+    @Override
+    public void close() throws Exception {
+        this.reader.close();
+    }
+
+    @Override
+    public Token nextToken() throws Lexception {
+        ignoreWhitespace();
+        beginNewToken();
+
+        if (Character.isJavaIdentifierStart(this.ch)) {
+            return identifierOrKeyword();
+        }
+
+        if (Character.isDigit(this.ch)) {
+            return integer();
+        }
+
+        return operatorOrSeparator();
+    }
+
+    private void ignoreWhitespace() {
+        while (Character.isWhitespace(this.ch)) {
+            advance();
+        }
+    }
+
+    private void beginNewToken() {
+        this.startPosition = this.reader.getColumn();
+        this.endPosition = this.startPosition;
+    }
+
+    private boolean isKeyword(String lexeme) {
+        try {
+            TokenKind.valueOf(lexeme);  
+            return true;  
+        } catch (IllegalArgumentException e) {
+            return false;  
+        }
+    }
+
+    private Token identifierOrKeyword() {
+        String lexeme = "";
+
+        do {
+            lexeme += this.ch;
+            advance();
+        } while (Character.isJavaIdentifierPart(this.ch) && !atEof());
+
+        return new Token(
+            SymbolTable.recordSymbol(lexeme, TokenKind.Identifier),
+            this.startPosition,
+            this.endPosition - 1,
+            reader.getLineNumber());
+    }
+
+    private Token integer() {
+        String lexeme = "";
+
+        do {
+            lexeme += this.ch;
+            advance();
+        } while (Character.isDigit(this.ch) && !atEof());
+
+        
+        return new Token(
+            SymbolTable.recordSymbol(lexeme, TokenKind.IntLit),
+            this.startPosition,
+            this.endPosition - 1,
+            reader.getLineNumber());
+    }  
+    
+    private Token operatorOrSeparator() throws Lexception {
+        String singleCharacter = "" + this.ch;
+
+        if (atEof()) {
+            return new Token(
+                SymbolTable.recordSymbol(singleCharacter, TokenKind.EOF),
+                this.startPosition,
+                this.endPosition,
+                this.lineNumber); 
+        }
+
+        advance();
+
+        String doubleCharacter = singleCharacter + this.ch;
+        Symbol symbol = SymbolTable.recordSymbol(doubleCharacter, TokenKind.BogusToken);
+
+        if (symbol == null) {
+            return singleCharacterOperatorOrSeparator(singleCharacter);
+        } else if (symbol.getTokenKind() == TokenKind.Comment) {
+            ignoreComment();
+            return nextToken();
+        } else {
+            advance();
+
+            return new Token(
+                symbol,
+                this.startPosition,
+                this.endPosition - 1,
+                this.lineNumber);
+        }
+    }
+
+    private Token singleCharacterOperatorOrSeparator(String lexeme) throws Lexception {
+        Symbol symbol = SymbolTable.recordSymbol(lexeme, TokenKind.BogusToken);
+
+        if (symbol == null) {
+            throw new Lexception(lexeme, this.reader.getLineNumber(), this.reader.getColumn());
+        } else {
+            return new Token(
+                symbol,
+                this.startPosition,
+                this.endPosition - 1,
+                this.lineNumber); 
+        }
+    }
+
+    private void ignoreComment() {
+        int currentLine = this.reader.getLineNumber();
+
+        while (currentLine == this.reader.getLineNumber() && !atEof()) {
+            advance();
+        }
+    }
+
+    private void advance() {
+        this.ch = this.reader.read();
+        this.endPosition++;
+    }
+
+    private boolean atEof() {
+        return this.ch == '\0';
+    }
+
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            System.err.println("usage: java lexer.Lexer filename.x");
+            System.exit(1);
+        }
+    
+        String sourceFilePath = args[0];
+        File file = new File(sourceFilePath);
+    
+        if (!file.exists() || !file.isFile()) {
+            sourceFilePath = "xsamples" + File.separator + args[0];
+            file = new File(sourceFilePath);
+        }
+
+    
+        if (!file.exists() || !file.isFile()) {
+            System.err.println("Error: File " + sourceFilePath + " does not exist or is not a valid file.");
+            System.exit(1);
+        }
+    
+        try (Lexer lexer = new Lexer(args[0])) {
+            Token token;
+    
+            while ((token = lexer.nextToken()).getTokenKind() != TokenKind.EOF) {
+                System.out.println(token);
+            }
+
+            System.out.println();
+            System.out.println(lexer);
+
+        } catch (Lexception lexception) {
+            System.err.println(lexception.getMessage());
+            System.exit(1);
+        } catch (Exception exception) {
+            System.err.println("Failed to close the Lexer");
+            System.exit(1);
+        }
+    
+        try (BufferedReader br = new BufferedReader(new FileReader(sourceFilePath))) {
+            String line;
+            int lineNumber = 1;
+            while ((line = br.readLine()) != null) {
+                System.out.printf("%3d: %s%n", lineNumber, line);
+                lineNumber++;
+            }
+        } catch (IOException e) {
+            System.err.println("Error reading the source file.");
+            System.exit(1);
+        }
+    }    
+}
---
+++ parser/Parser.java
@@ -0,0 +1,494 @@
+package parser;
+
+import java.util.EnumSet;
+
+import ast.AST;
+import ast.trees.*;
+import lexer.ILexer;
+import lexer.Lexception;
+import lexer.Lexer;
+import lexer.daos.Token;
+import lexer.daos.TokenKind;
+
+public class Parser {
+  private Token currentToken;
+  private ILexer lexer;
+
+  private EnumSet<TokenKind> relationalOperators = EnumSet.of(
+      TokenKind.Equal,
+      TokenKind.NotEqual,
+      TokenKind.Less,
+      TokenKind.LessEqual,
+      TokenKind.Greater,
+      TokenKind.GreaterEqual);
+  private EnumSet<TokenKind> additionOperators = EnumSet.of(
+      TokenKind.Plus,
+      TokenKind.Minus,
+      TokenKind.Or,
+      TokenKind.BoolOr,
+      TokenKind.BoolXor);
+  private EnumSet<TokenKind> multiplicationOperators = EnumSet.of(
+      TokenKind.Multiply,
+      TokenKind.Divide,
+      TokenKind.And,
+      TokenKind.BoolAnd);
+
+  public Parser(String sourceProgramPath) throws Lexception {
+    this(new Lexer(sourceProgramPath));
+  }
+
+  public Parser(ILexer lexer) throws Lexception {
+    this.lexer = lexer;
+    scan();
+  }
+
+  private void expect(TokenKind expected) throws SyntaxErrorException, Lexception {
+    if (this.currentToken.getTokenKind() == expected) {
+      scan();
+    } else {
+      error(currentToken.getTokenKind(), expected);
+    }
+  }
+
+  private void error(TokenKind actual, TokenKind... expected) throws SyntaxErrorException {
+    throw new SyntaxErrorException(actual, expected);
+  }
+
+  private void scan() throws Lexception {
+    this.currentToken = lexer.nextToken();
+  }
+
+  private boolean match(TokenKind... kinds) {
+    for (TokenKind tokenKind : kinds) {
+      if (this.currentToken.getTokenKind() == tokenKind) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  public AST execute() throws SyntaxErrorException, Lexception {
+    return program();
+  }
+
+  /**
+   * PROGRAM → 'program' BLOCK
+   *
+   * @throws Lexception
+   * @throws SyntaxErrorException
+   */
+  private AST program() throws SyntaxErrorException, Lexception {
+    AST node = new ProgramTree();
+
+    expect(TokenKind.Program);
+    node.addChild(block());
+
+    return node;
+  }
+
+  /**
+   * BLOCK → '{' DECLARATIONS* STATEMENTS* '}'
+   *
+   * @throws Lexception
+   * @throws SyntaxErrorException
+   */
+  private AST block() throws SyntaxErrorException, Lexception {
+    AST node = new BlockTree();
+
+    expect(TokenKind.LeftBrace);
+
+    while (startingDeclaration()) {
+      node.addChild(declaration());
+    }
+
+    while (startingStatement()) {
+      node.addChild(statement());
+    }
+
+    expect(TokenKind.RightBrace);
+
+    return node;
+  }
+
+  private boolean startingDeclaration() {
+    return match(
+        TokenKind.IntType,
+        TokenKind.BooleanType,
+        TokenKind.CharType,
+        TokenKind.BinaryType);
+  }
+
+  private boolean startingStatement() {
+    return match(
+        TokenKind.If,
+        TokenKind.While,
+        TokenKind.Return,
+        TokenKind.LeftBrace,
+        TokenKind.Identifier,
+        TokenKind.Iterate);
+  }
+
+  /**
+   * DECLARATION → TYPE NAME
+   * DECLARATION → TYPE NAME '(' FORMALS ')' BLOCK
+   */
+  private AST declaration() throws SyntaxErrorException, Lexception {
+    AST type = type();
+    AST name = name();
+
+    if (match(TokenKind.LeftParen)) {
+      AST function = new FunctionDeclarationTree().addChild(type).addChild(name);
+
+      expect(TokenKind.LeftParen);
+      function.addChild(formals());
+      expect(TokenKind.RightParen);
+
+      return function.addChild(block());
+    } else {
+      return new DeclarationTree().addChild(type).addChild(name);
+    }
+  }
+
+  /**
+   * TYPE → 'int'
+   * TYPE → 'boolean'
+   */
+  private AST type() throws Lexception, SyntaxErrorException {
+    AST node = null;
+
+    if (match(TokenKind.IntType)) {
+      node = new IntTypeTree();
+    } else if (match(TokenKind.BooleanType)) {
+        node = new BoolTypeTree();
+    } else if (match(TokenKind.BinaryType)) { 
+        node = new BinaryTypeTree(); 
+    } else if (match(TokenKind.CharType)) { 
+        node = new CharTypeTree(); 
+    } else {
+        error(currentToken.getTokenKind(), TokenKind.IntType, TokenKind.BooleanType); 
+    }
+
+    scan();
+    return node;
+  }
+
+  /*
+   * NAME → <id>
+   */
+  private AST name() throws Lexception, SyntaxErrorException {
+    AST node = null;
+
+    if (match(TokenKind.Identifier)) {
+      node = new IdentifierTree(currentToken);
+
+      expect(TokenKind.Identifier);
+    }
+
+    return node;
+  }
+
+  /**
+   * FORMALS → 𝜀
+   * FORMALS → DECLARATION (',' DECLARATION)*
+   */
+  private AST formals() throws SyntaxErrorException, Lexception {
+    AST formals = new FormalsTree();
+
+    if (match(TokenKind.RightParen)) {
+      return formals;
+    } else {
+      do {
+        formals.addChild(declaration());
+
+        if (match(TokenKind.Comma)) {
+          expect(TokenKind.Comma);
+        }
+      } while (!match(TokenKind.RightParen));
+
+      return formals;
+    }
+  }
+
+  /**
+   * STATEMENT → 'if' E 'then' BLOCK 'else' BLOCK
+   * STATEMENT → 'while' E BLOCK
+   * STATEMENT → 'return' E
+   * STATEMENT → BLOCK
+   * STATEMENT → NAME '=' E
+   */
+  private AST statement() throws SyntaxErrorException, Lexception {
+
+    if (match(TokenKind.Iterate)) {
+      return iterateStatement();
+  }
+
+    switch (currentToken.getTokenKind()) {
+        case If: {
+            return ifStatement();
+        }
+        case While: {
+            return whileStatement();
+        }
+        case Return: {
+            return returnStatement();
+        }
+        case LeftBrace: {
+            return block();
+        }
+        case Identifier: {
+            return assignStatement();
+        }
+        case Iterate: {
+          return iterateStatement();
+      }
+        default:
+            error(currentToken.getTokenKind(), TokenKind.If, TokenKind.While, TokenKind.Return, TokenKind.LeftBrace, TokenKind.Identifier, TokenKind.Iterate);
+            return null;
+    }
+}
+
+
+private AST range() throws SyntaxErrorException, Lexception {
+    AST node = new RangeTree();
+
+    node.addChild(expression());
+    expect(TokenKind.Tilde);
+    node.addChild(expression());
+
+    return node;
+}
+
+
+  /**
+   * STATEMENT → 'if' E 'then' BLOCK 'else' BLOCK
+   */
+  private AST ifStatement() throws SyntaxErrorException, Lexception {
+    AST node = new IfTree();
+
+    expect(TokenKind.If);
+    node.addChild(expression());
+    expect(TokenKind.Then);
+    node.addChild(block());
+    expect(TokenKind.Else);
+    node.addChild(block());
+
+    return node;
+  }
+
+  /**
+   * STATEMENT → 'while' E BLOCK
+   */
+  private AST whileStatement() throws SyntaxErrorException, Lexception {
+    AST node = new WhileTree();
+
+    expect(TokenKind.While);
+    node.addChild(expression()).addChild(block());
+
+    return node;
+  }
+
+  /**
+   * STATEMENT → 'return' E
+   */
+
+  private AST returnStatement() throws SyntaxErrorException, Lexception {
+    AST node = new ReturnTree();
+
+    expect(TokenKind.Return);
+
+    node.addChild(expression());
+    return node;
+  }
+  
+  private AST iterateStatement() throws SyntaxErrorException, Lexception {
+    
+    AST node = new IterationTree();
+
+    expect(TokenKind.Iterate);
+    expect(TokenKind.Pipette); 
+    node.addChild(range());
+    node.addChild(block());
+
+    return node;
+    
+}
+
+  /**
+   * STATEMENT → NAME '=' E
+   */
+
+  private AST assignStatement() throws SyntaxErrorException, Lexception {
+    AST node = new AssignmentTree();
+
+    node.addChild(name());
+    expect(TokenKind.Assign);
+    node.addChild(expression());
+
+    return node;
+  }
+
+  /**
+   * E → SE
+   * E → SE '==' SE
+   * E → SE '!=' SE
+   * E → SE '<' SE
+   * E → SE '<=' SE
+   */
+  private AST expression() throws Lexception, SyntaxErrorException {
+    AST tree, child = simpleExpression();
+
+    while ((tree = getRelopTree()) != null) {
+        tree.addChild(child);
+        tree.addChild(simpleExpression());
+        child = tree;
+    }
+
+    return child;
+}
+
+private AST getRelopTree() throws Lexception {
+    if (relationalOperators.contains(currentToken.getTokenKind()) || 
+        currentToken.getTokenKind() == TokenKind.Greater ||
+        currentToken.getTokenKind() == TokenKind.GreaterEqual) {
+        AST tree = new RelOpTree(currentToken);
+        scan();
+
+        return tree;
+    } else {
+        return null;
+    }
+}
+
+  /**
+   * SE → T
+   * SE → SE '+' T
+   * SE → SE '-' T
+   * SE → SE '|' T
+   */
+  private AST simpleExpression() throws Lexception, SyntaxErrorException {
+    AST tree, child = term();
+
+    while ((tree = getAddOpTree()) != null) {
+      tree.addChild(child);
+      tree.addChild(term());
+
+      child = tree;
+    }
+
+    return child;
+  }
+
+  private AST getAddOpTree() throws Lexception {
+    if (additionOperators.contains(currentToken.getTokenKind())) {
+      AST tree = new AddOpTree(currentToken);
+      scan();
+
+      return tree;
+    } else {
+      return null;
+    }
+  }
+
+  /**
+   * T → F
+   * T → T '*' F
+   * T → T '/' F
+   * T → T '&' F
+   */
+  private AST term() throws SyntaxErrorException, Lexception {
+    AST tree, child = factor();
+
+    while ((tree = getMultOpTree()) != null) {
+      tree.addChild(child);
+      tree.addChild(factor());
+
+      child = tree;
+    }
+
+    return child;
+  }
+
+  private AST getMultOpTree() throws Lexception {
+    if (multiplicationOperators.contains(currentToken.getTokenKind())) {
+      AST tree = new MultOpTree(currentToken);
+      scan();
+
+      return tree;
+    } else {
+      return null;
+    }
+  }
+
+  /**
+   * F → '(' E ')'
+   * F → <int>
+   * F → NAME
+   * F → NAME '(' E_LIST ')'
+   */
+
+ private AST factor() throws SyntaxErrorException, Lexception {
+    switch (currentToken.getTokenKind()) {
+        case LeftParen: {
+            expect(TokenKind.LeftParen);
+            AST node = expression();
+            expect(TokenKind.RightParen);
+            return node;
+        }
+        case IntLit: {
+            AST node = new IntTree(currentToken);
+            expect(TokenKind.IntLit);
+            return node;
+        }
+        case BinaryLit: {  
+            AST node = new BinaryLitTree(currentToken);
+            expect(TokenKind.BinaryLit);
+            return node;
+        }
+        case CharLit: {  
+            AST node = new CharLitTree(currentToken);
+            expect(TokenKind.CharLit);
+            return node;
+        }
+        case Identifier: {
+            AST node = new IdentifierTree(currentToken);
+            expect(TokenKind.Identifier);
+            if (match(TokenKind.LeftParen)) {
+                node = new CallTree().addChild(node);
+                expect(TokenKind.LeftParen);
+                node.addChild(actualArguments());
+                expect(TokenKind.RightParen);
+            }
+            return node;
+        }
+        default:
+            error(
+                currentToken.getTokenKind(),
+                TokenKind.LeftParen,
+                TokenKind.IntLit,
+                TokenKind.BinaryLit,  
+                TokenKind.CharLit,    
+                TokenKind.Identifier);
+            return null;
+    }
+}
+
+  /**
+   * ACTUAL_ARGUMENTS → 𝜀
+   * ACTUAL_ARGUMENTS → E (',' E)*
+   */
+  private AST actualArguments() throws SyntaxErrorException, Lexception {
+    AST node = new ActualArgumentsTree();
+
+    while (!match(TokenKind.RightParen)) {
+      node.addChild(expression());
+
+      if (match(TokenKind.Comma)) {
+        expect(TokenKind.Comma);
+      }
+    }
+
+    return node;
+  }
+}
+
